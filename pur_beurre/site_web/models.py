f1c54b6eaf05574bd2ccdaf75ec3d840d5ff206d
Clélio Favoccia <favoccia.c@live.fr>
2021-01-22 10:05:59 +0100

    Modifications sur models.py

--- pur_beurre/site_web/models.py
+++ pur_beurre/site_web/models.py
@@ -2,30 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Nutriscore(models.Model):
-    """Nutriscores of products"""
-
-    # Fields
-    value = models.CharField(max_length=1)
-
-    def __str__(self):
-        """Print attribute as title's object in admin"""
-        return self.value
-
-
 class Category(models.Model):
     """Categories of products"""
-
-    # Fields
-    name = models.CharField()
-
-    def __str__(self):
-        """Print attribute as title's object in admin"""
-        return self.name
-
-
-class Store(models.Model):
-    """Stores where it's possible to find the product"""
 
     # Fields
     name = models.CharField()
@@ -41,9 +19,9 @@ class Product(models.Model):
     # Fields
     name = models.CharField()
     description = models.TextField()
-    stores = models.ForeignKey(Store, on_delete=models.CASCADE)
-    categories = models.ManyToManyField(Category, on_delete=models.CASCADE)
-    nutriscore = models.ForeignKey(Nutriscore, on_delete=models.CASCADE)
+    nutriscore = models.CharField(max_length=1)
+    stores = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         """Print attribute as title's object in admin"""
